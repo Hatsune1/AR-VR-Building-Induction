7514df287f3f7496ec5143602eeed1f21b98b172
Hatsune1 <15210065800@163.com>
2019-01-28 12:23:52 +0000

    Unity files added

---
+++ Unity file/VR TOUR/Library/Collab/Download/Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CameraController : MonoBehaviour
+{
+
+    public Button receptionButton, mainAreaButton;
+
+    Vector3[] positions = new[] {
+        new Vector3(0, 0, 0),
+        new Vector3(3, 0, 0),
+        new Vector3(6, 0, 0) };
+
+
+    // Use this for initialization
+    void Start()
+    {
+        receptionButton.onClick.AddListener(delegate { TransportToPositionNo(0); });
+        mainAreaButton.onClick.AddListener(delegate { TransportToPositionNo(1); });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void TransportToPositionNo(int positionNo)
+    {
+        Vector3 distance = positions[positionNo] - transform.position;
+        transform.position += distance;
+    }
+}
