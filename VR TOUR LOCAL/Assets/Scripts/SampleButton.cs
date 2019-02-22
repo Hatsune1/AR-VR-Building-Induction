2c9f26a463ba613c1ad2df57411aa1a2435a1d4d
Hatsune1 <15210065800@163.com>
2019-02-22 12:50:11 +0000

    UI design file

---
+++ VR TOUR LOCAL/Assets/Scripts/SampleButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SampleButton : MonoBehaviour {
+
+    public Button button;
+    public Text locationNumber;
+    public Text locationName;
+    //public Text details;
+    //public Image iconImage; // add icon to different types of location.
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
